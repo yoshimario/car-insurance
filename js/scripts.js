b10910d6fd29d7f8dc60fc6b2b01fd1a68cb579d
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-13 14:33:53 -0700

    it will make a working car insurance quotes site
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,17 @@ $(document).ready(function() {
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
 
-    let quote = (100 - age) * 3;
-    if(gender==='male'&& age <26){
-      quote+=50;
+    if (age) {
+      let quote = (100 - age) * 3;
+      if (gender === 'male' && age < 26) {
+        quote += 50;
+      }
+
+      $("#rate").text(quote);
+      $("#quote").show();
+    } else {
+      alert('Please enter your age.');
     }
-
-    $("#rate").text(quote);
-    $("#quote").show();
-
 
     event.preventDefault();
   });

6c4962ec56e3b4f7e3fb1d500e2f04254a7b7518
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-13 14:26:39 -0700

    it will add if statement so that if conditions met money added to quote
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("form#insurance").submit(function(event) {
+    const age = parseInt($("input#age").val());
+    const gender = $("select#gender").val();
+
+    let quote = (100 - age) * 3;
+    if(gender==='male'&& age <26){
+      quote+=50;
+    }
+
+    $("#rate").text(quote);
+    $("#quote").show();
+
+
+    event.preventDefault();
+  });
+});
